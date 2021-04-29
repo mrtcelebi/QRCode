ae4d9c56740822167e028a669eefb18c37b22259
Murat Celebi <mrttcelebi@gmail.com>
2021-04-29 09:08:41 +0300

    Initial Commit

---
+++ QR-Code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QR-Code
+//
+//  Created by Murat Celebi on 29.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
