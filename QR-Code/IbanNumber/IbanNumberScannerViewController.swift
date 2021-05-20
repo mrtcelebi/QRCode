71d10b92c70816e78cd18d15cbfd1df0a526b76c
Murat Celebi <mrttcelebi@gmail.com>
2021-05-20 13:13:58 +0300

    remove boxes

--- QR-Code/IbanNumber/IbanNumberScannerViewController.swift
+++ QR-Code/IbanNumber/IbanNumberScannerViewController.swift
@@ -14,11 +14,8 @@ class IbanNumberScannerViewController: BaseIbanNumberScannerViewController {
     var request: VNRecognizeTextRequest!
     // Temporal string tracker
     let numberTracker = StringTracker()
-    var boxLayer = [CAShapeLayer]()
     var getIbanNumber: ((String) -> Void)?
-    
-    typealias ColoredBoxGroup = (color: CGColor, boxes: [CGRect])
-    
+        
     override func viewDidLoad() {
         request = VNRecognizeTextRequest(completionHandler: recognizeTextHandler)
         
@@ -29,8 +26,6 @@ class IbanNumberScannerViewController: BaseIbanNumberScannerViewController {
     
     private func recognizeTextHandler(request: VNRequest, error: Error?) {
         var numbers = [String]()
-        var redBoxes = [CGRect]()
-        var greenBoxes = [CGRect]()
         
         guard let results = request.results as? [VNRecognizedTextObservation] else {
             return
@@ -41,24 +36,14 @@ class IbanNumberScannerViewController: BaseIbanNumberScannerViewController {
         for visionResult in results {
             guard let candidate = visionResult.topCandidates(maximumCandidates).first else { continue }
             
-            var numberIsSubstring = true
             
             if let result = candidate.string.extractIbanNumber() {
-                let (range, number) = result
-                
-                if let box = try? candidate.boundingBox(for: range)?.boundingBox {
+                let (_, number) = result
                     numbers.append(number)
-                    greenBoxes.append(box)
-                    numberIsSubstring = !(range.lowerBound == candidate.string.startIndex && range.upperBound == candidate.string.endIndex)
-                }
-            }
-            if numberIsSubstring {
-                redBoxes.append(visionResult.boundingBox)
             }
         }
         
         numberTracker.logFrame(strings: numbers)
-        show(boxGroups: [(color: UIColor.red.cgColor, boxes: redBoxes), (color: UIColor.green.cgColor, boxes: greenBoxes)])
         
         if let sureNumber = numberTracker.getStableString() {
             stopRunning()
@@ -84,37 +69,5 @@ class IbanNumberScannerViewController: BaseIbanNumberScannerViewController {
             }
         }
     }
-    
-    // MARK: - Bounding box drawing
-    
-    private func draw(rect: CGRect, color: CGColor) {
-        let layer = CAShapeLayer()
-        layer.opacity = 0.5
-        layer.borderColor = color
-        layer.borderWidth = 1
-        layer.frame = rect
-        boxLayer.append(layer)
-        previewView.videoPreviewLayer.insertSublayer(layer, at: 1)
-    }
-    
-    private func removeBoxes() {
-        for layer in boxLayer {
-            layer.removeFromSuperlayer()
-        }
-        boxLayer.removeAll()
-    }
-    
-    private func show(boxGroups: [ColoredBoxGroup]) {
-        DispatchQueue.main.async {
-            let layer = self.previewView.videoPreviewLayer
-            self.removeBoxes()
-            for boxGroup in boxGroups {
-                let color = boxGroup.color
-                for box in boxGroup.boxes {
-                    let rect = layer.layerRectConverted(fromMetadataOutputRect: box.applying(self.visionToAVFTransform))
-                    self.draw(rect: rect, color: color)
-                }
-            }
-        }
-    }
+
 }
